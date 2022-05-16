5d3e99b1a11fb78bf76665640acfe3a19297f104
KjROX <karnamandal380@gmail.com>
2022-05-16 11:52:38 +0530

    updation of images in main modal images from thumbnail images

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const modalWindow = document.querySelector(".modal-outer");
 const crossSymbol = document.querySelector(".close-icon");
 const previousButton = document.querySelector(".previous-icon");
 const nextButton = document.querySelector(".next-icon");
+const images = Array.from(document.querySelectorAll(".thumbnail"));
+
 //Functions
 
 function openModal() {
@@ -15,7 +17,25 @@ function openModal() {
 function closeModal() {
   modalWindow.classList.remove("open");
 }
-function imageUpdation() {}
+function thumbnailToMainImage(src) {
+  return src.replace("-thumbnail", "");
+}
+
+function imageUpdation(src) {
+  modalMainImage.src = thumbnailToMainImage(src);
+}
+
+function handleImageClick(e) {
+  images.forEach((image) => {
+    if (image.classList.contains("opacity")) {
+      image.classList.remove("opacity");
+    }
+  });
+  e.currentTarget.classList.add("opacity");
+  const { src } = e.currentTarget;
+  imageUpdation(src);
+}
+
 //eventlisteners
 
 mainHeroImage.addEventListener("click", openModal);
@@ -25,15 +45,14 @@ window.addEventListener("keyup", (e) => {
     closeModal();
   }
 });
-// nextButton.addEventListener("click", (e) => {
-//   const parentEl = e.currentTarget.parentElement;
-//   const currentEl = modalMainImage;
-//   if (currentEl) console.log(currentEl);
-//   //   console.log(e);
-// });
+
 modalWindow.addEventListener("click", (e) => {
   const clickOutside = !e.target.closest(".hero-image.modal");
   if (clickOutside) {
     closeModal();
   }
+});
+
+images.forEach((image) => {
+  image.addEventListener("click", handleImageClick);
 });
